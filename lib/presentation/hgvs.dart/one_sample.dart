f30fd4cf55580d1d6eb50f4355272bf32ee1e9bc
Varun pc <Varunrevathi6@gmail.com>
2024-02-16 16:59:31 +0530

    sample Again

---
+++ lib/presentation/hgvs.dart/one_sample.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Hai extends StatelessWidget {
+  const Hai({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
